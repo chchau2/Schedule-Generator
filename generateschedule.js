cdde55bd52833ecccc7bfec874b33604aeb32203
Heidi Chan <heidi.chan@berkeley.edu>
2024-12-31 15:53:45 -0800

    connection for input of the days between input and js behind the scene

---
+++ generateschedule.js
@@ -0,0 +1,24 @@
+let chrisNonWorkingDays;
+let janiceNonWorkingDays;
+let natalieNonWorkingDays;
+let megNonWorkingDays;
+let christineNonWorkingDays;
+let abrahamNonWorkingDays;
+let vickiNonWorkingDays;
+
+document.getElementById("generateScheduleButton").onclick = function() {
+    chrisNonWorkingDays = document.getElementById("chrisHours").value;
+    janiceNonWorkingDays = document.getElementById("janiceHours").value;
+    natalieNonWorkingDays = document.getElementById("natalieHours").value;
+    megNonWorkingDays = document.getElementById("megHours").value;
+    christineNonWorkingDays = document.getElementById("christineHours").value;
+    abrahamNonWorkingDays = document.getElementById("abrahamHours").value;
+    vickiNonWorkingDays = document.getElementById("vickiHours").value;
+    console.log(chrisNonWorkingDays);
+    console.log(janiceNonWorkingDays);
+    console.log(natalieNonWorkingDays);
+    console.log(megNonWorkingDays);
+    console.log(christineNonWorkingDays);
+    console.log(abrahamNonWorkingDays);
+    console.log(vickiNonWorkingDays);
+}
